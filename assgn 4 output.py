2ed69fe94faa3c473f2cc18adea8897717b97b31
Vishwas5933 <97469817+Vishwas5933@users.noreply.github.com>
2022-03-01 19:23:11 +0530

    Add files via upload

---
+++ assgn 4 output.py
@@ -0,0 +1,81 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:/python3.10/ASSGN 4 QUES 1.py ===================
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+=================== RESTART: C:/python3.10/ques 2 assgn 4.py ===================
+Enter the number of rows in Pascal's Triangle: 8
+
+Using recursions:
+
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+
+Using loops:
+
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+
+
+=================== RESTART: C:/python3.10/ques 3 assgn 4.py ===================
+Enter the first integer: 4
+Enter the first integer: 3
+Remainder is:  1
+Quotient is:  1
+Both values are non zero
+{5}
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set:  frozenset({5, 6, 7})
+Largest value in the set:  7
+Hash value:  7
+
+
+============================================================= RESTART: C:/python3.10/ques 4 assgn 4.py ============================================================
+Object created
+The name of the student it vishwas and SID is 21104064.
+Object destroyed
+
+
+============================================================= RESTART: C:/python3.10/ques 5 assgn 4.py ============================================================
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+
+============================================================= RESTART: C:/python3.10/ques 6 assgn 4.py ============================================================
+Enter the first word: pan
+
+Enter a new meaningful word to test your friendship: nap
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+
